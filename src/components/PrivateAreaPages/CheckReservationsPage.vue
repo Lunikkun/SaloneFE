f700ffe85a8df504bf0739474d997d4e4bae7411
Antonio <antoniosica0710@gmail.com>
2025-10-29 00:15:10 +0100

    Styling della lista prenotazion

--- src/components/PrivateAreaPages/CheckReservationsPage.vue
+++ src/components/PrivateAreaPages/CheckReservationsPage.vue
@@ -4,15 +4,16 @@ import ReservationCard from './ReservationCard.vue';
 import type Reservation from '@/interfaces/Reservation';
 
 const userReservation = ref<Reservation[]>()
-let result
 onMounted(async()=>{
     let data = await fetch("http://127.0.0.1:3000/user/prenotazioniutente", {credentials:'include'})
     userReservation.value = await data.json()
-    console.log(result)
+    console.log(userReservation)
 })
 </script>
 <template>
-    <div class="flex flex-col justify-evenly items-start">
+    <div class="flex flex-col border-4 w-full h-full gap-y-2">
+        <hr class="w-full border-2"/>
+        <div>LISTA PRENOTAZIONI</div>
         <div :key="elem.id" v-for="elem in userReservation">
             <ReservationCard :reservation="elem"/>
         </div>
